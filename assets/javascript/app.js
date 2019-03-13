866b90da440b59c21f78cc7019a7375c1e701a39
Charles Ladesic <cladesic.student@gmail.com>
2019-03-13 18:34:31 -0500

    fixed active

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,32 +47,33 @@ $(document).ready(function () {
                         var p = $("<p>").text("Rating: " + rating);
                         var topicImage = $("<img>");
                         topicImage.attr("src", results[i].images.fixed_height_still.url); 
-                        still= results[i].images.fixed_height_still.url; 
-                        active= results[i].images.fixed_height.url;     
+                        topicImage.addClass("topicGif");
+                        topicImage.attr("data-state", "still");
+                        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+                        topicImage.attr("data-animate", results[i].images.fixed_height.url );
                         gifDiv.append(p);
                         gifDiv.append(topicImage);
                         $("#gifs-return").prepend(gifDiv);
-                       //console.log(still);
-                      // console.log(topicImage);
+
                     }
                     
                 }
                 
             });
     });
-    $(document).on("click", "img", function () {
-       var src = still;
-        if (still !== "s.gif"){
-          $(this).attr('src', src.replace(still, active));
-            src = active;
-          // console.log("Made it to if statement" + this);
-       }else 
-       $(this).attr('src', src.replace(active, still));
-        src = still;
-       //console.log("else statement");
+    $(document).on("click", ".topicGif", function () {
        
+        var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
        
     
-    });
+    
 
 });
